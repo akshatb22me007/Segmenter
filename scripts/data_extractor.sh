f22d5ad5b28acdd10d0dfe2ea523b7cf506519f0
akshatjain0107 <akshat.jain@hellozelo.com>
2025-02-09 01:21:38 +0530

    Added: data_extractor bash script

---
+++ scripts/data_extractor.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [[ ! -f ~/.kaggle/kaggle.json ]]; then
+  echo -n "Kaggle username: "
+  read USERNAME
+  echo
+  echo -n "Kaggle API key: "
+  read APIKEY
+
+  mkdir -p ~/.kaggle
+  echo "{\"username\":\"$USERNAME\",\"key\":\"$APIKEY\"}" > ~/.kaggle/kaggle.json
+  chmod 600 ~/.kaggle/kaggle.json
+fi
+
+uv pip install kaggle --upgrade
+cd ..
+mkdir data
+cd data
+mkdir imgs
+mkdir masks
+cd ..
+cd scripts
+kaggle competitions download -c carvana-image-masking-challenge -f train_hq.zip
+unzip train_hq.zip
+mv train_hq/* ../data/imgs/
+rm -d train_hq
+rm train_hq.zip
+
+kaggle competitions download -c carvana-image-masking-challenge -f train_masks.zip
+unzip train_masks.zip
+mv train_masks/* ../data/masks/
+rm -d train_masks
+rm train_masks.zip
